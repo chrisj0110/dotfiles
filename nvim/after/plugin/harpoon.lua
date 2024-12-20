236bffac223a0ef71664f4cbcd6b871ecc41fdde
chrisj0110 <45946825+chrisj0110@users.noreply.github.com>
2024-12-20 10:21:46 -0600

    fix mapping conflicts

--- nvim/after/plugin/harpoon.lua
+++ nvim/after/plugin/harpoon.lua
@@ -6,9 +6,9 @@ harpoon:setup({
     },
 })
 
-vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
-vim.keymap.set("n", "T", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-vim.keymap.set("n", "<leader>hc", function()  harpoon:list():clear() end)
+vim.keymap.set("n", "<leader>ha", function() harpoon:list():add() end, {desc = "add to harpoon"})
+vim.keymap.set("n", "<leader>hl", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, {desc = "harpoon list"})
+vim.keymap.set("n", "<leader>hc", function()  harpoon:list():clear() end, {desc = "clear harpoon"})
 
 -- Set <leader>1..<leader>9 be my shortcuts to moving to the files
 for _, idx in ipairs { 1, 2, 3, 4, 5, 6, 7, 8, 9 } do
@@ -17,15 +17,4 @@ for _, idx in ipairs { 1, 2, 3, 4, 5, 6, 7, 8, 9 } do
         harpoon:list()._index = idx
     end)
 end
-
--- swap between windows 1 and 2
-vim.keymap.set("n", "t", function()
-    if harpoon:list()._index == 1 then
-        harpoon:list():select(2)
-        harpoon:list()._index = 2
-    else
-        harpoon:list():select(1)
-        harpoon:list()._index = 1
-    end
-end)
 
