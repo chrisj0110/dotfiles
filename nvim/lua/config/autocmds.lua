a357639b4fbc4079b896c2373e30fc6e13180cfd
chrisj0110 <45946825+chrisj0110@users.noreply.github.com>
2024-12-22 15:36:38 -0600

    don't automatically comment a new line following a comment

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -6,3 +6,13 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+vim.api.nvim_create_augroup("CustomFormatOptions", { clear = true })
+vim.api.nvim_create_autocmd("FileType", {
+  group = "CustomFormatOptions",
+  pattern = "*",
+  callback = function()
+    vim.opt_local.formatoptions:remove("c") -- disable Automatically continue comments when pressing Enter in Insert mode
+    vim.opt_local.formatoptions:remove("o") -- disable Allow automatic continuation of comments when using the o or O commands.
+  end,
+})
