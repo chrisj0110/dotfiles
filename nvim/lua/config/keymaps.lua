8b640aa2097df37af2c9a4a8afcc832b13ee8d98
chrisj0110 <45946825+chrisj0110@users.noreply.github.com>
2024-12-22 14:52:46 -0600

    remap Y to yy

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Remap Y to behave like yy
+vim.keymap.set("n", "Y", "yy", { desc = "Yank entire line (like yy)" })
