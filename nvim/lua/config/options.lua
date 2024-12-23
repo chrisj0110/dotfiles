36a301e3c10508783bf67a842e681f9a9d411029
chrisj0110 <45946825+chrisj0110@users.noreply.github.com>
2024-12-23 09:08:46 -0600

    bring in options from before lazy

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -4,3 +4,27 @@
 
 vim.opt.clipboard = "" -- don't yank to clipboard
 vim.g.autoformat = false
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.isfname:append("@-@")
+
+-- skipping while I try lazyvim defaults:
+-- vim.opt.smartindent = true
+-- vim.opt.scrolloff = 8
+-- vim.opt.updatetime = 50
+-- vim.opt.fixendofline = false
+
