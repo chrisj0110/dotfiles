5f6d08e580649ea1222c26e4b5eaa02092ff2729
chrisj0110 <45946825+chrisj0110@users.noreply.github.com>
2024-12-23 11:33:27 -0600

    fewer warnings

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,6 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+vim = vim
+
 vim.opt.clipboard = "" -- don't yank to clipboard
 vim.g.autoformat = false
 
