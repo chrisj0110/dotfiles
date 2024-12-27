df64019b1f1e765bdc145f437df618472563027c
chrisj0110 <45946825+chrisj0110@users.noreply.github.com>
2024-12-27 09:33:30 -0600

    fix fugitive config issue

--- nvim/lua/plugins/fugitive.lua
+++ nvim/lua/plugins/fugitive.lua
@@ -1,4 +1,20 @@
-vim.keymap.set("n", "<leader>gm", ":!git checkout master -- %<CR>");
+return {
+    {
+        "tpope/vim-fugitive",
+        config = function()
+            vim.keymap.set("n", "<leader>gm", ":!git checkout master -- %<CR>")
+        end,
+    },
+    {
+        "shumphrey/fugitive-gitlab.vim",
+        config = function()
+            local gitlab_domain = os.getenv("WORK_GITLAB_DOMAIN")
+            if gitlab_domain then
+                vim.g.fugitive_gitlab_domains = { gitlab_domain }
+            else
+                vim.notify("WORK_GITLAB_DOMAIN is not set!", vim.log.levels.WARN)
+            end
+        end,
+    },
+}
 
-local gitlab_domain = os.getenv("WORK_GITLAB_DOMAIN")
-vim.g.fugitive_gitlab_domains = {gitlab_domain}
