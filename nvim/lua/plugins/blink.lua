d265a98fdf53458b7f628b64b11dc1cb3d70b66e
chrisj0110 <45946825+chrisj0110@users.noreply.github.com>
2025-01-05 12:44:35 -0600

    fix path autocomplete

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -29,9 +29,10 @@ return {
 
     -- Default list of enabled providers defined so that you can extend it
     -- elsewhere in your config, without redefining it, due to `opts_extend`
-    -- sources = {
-    --   default = { 'lsp', 'path', 'snippets', 'buffer' },
-    -- },
+    sources = {
+      default = { 'lsp', 'path', 'snippets', 'buffer' },
+      cmdline = { 'path' },
+    },
   },
   opts_extend = { "sources.default" }
 }
