bb295e4099b8a5d77853898ea5a022d065941c70
chrisj0110 <45946825+chrisj0110@users.noreply.github.com>
2025-01-07 11:10:46 -0600

    slow down animation

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -1,6 +1,13 @@
 return {
     "folke/snacks.nvim",
     opts = {
-        scroll = { },
+        scroll = {
+            animate = {
+                duration = {
+                    step = 15,
+                    total = 50, -- default is 100 - too slow for me
+                },
+            },
+        },
     }
 }
