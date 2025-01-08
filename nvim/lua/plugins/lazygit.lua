a748347401e701549f5454ce6c44b168fb74c62d
chrisj0110 <45946825+chrisj0110@users.noreply.github.com>
2025-01-08 10:50:26 -0600

    leader-l for lazygit

--- nvim/lua/plugins/lazygit.lua
+++ nvim/lua/plugins/lazygit.lua
@@ -8,6 +8,6 @@ return {
         vim.g.lazygit_use_custom_config_file_path = 1
         vim.g.lazygit_config_file_path = { os.getenv("HOME") .. "/.config/lazygit/config.yml" }
 
-        vim.keymap.set("n", "<leader>lg", "<cmd>LazyGit<cr>", {desc = "open lazygit (cwd)"})
+        vim.keymap.set("n", "<leader>l", "<cmd>LazyGit<cr>", {desc = "open lazygit (cwd)"})
     end
 }
