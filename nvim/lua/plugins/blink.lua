0d80098301fcb83476d37728f23dcf6f4e0a0b0e
chrisj0110 <45946825+chrisj0110@users.noreply.github.com>
2025-01-08 10:42:19 -0600

    update blink, integrate luasnip

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -10,6 +10,7 @@ return {
   -- If you use nix, you can build from source using latest nightly rust with:
   -- build = 'nix run .#build-plugin',
 
+  dependencies = { 'L3MON4D3/LuaSnip', version = 'v2.*' },
   opts = {
     -- 'default' for mappings similar to built-in completion
     -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
@@ -26,6 +27,8 @@ return {
       -- Adjusts spacing to ensure icons are aligned
       nerd_font_variant = 'mono'
     },
+
+    snippets = { preset = 'luasnip' },
 
     -- Default list of enabled providers defined so that you can extend it
     -- elsewhere in your config, without redefining it, due to `opts_extend`
