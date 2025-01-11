73505fbecd1e0c0d17a0b2ef9b38602f5c20bd43
chrisj0110 <45946825+chrisj0110@users.noreply.github.com>
2025-01-11 17:08:33 -0600

    use my new notes-slider plugin

---
+++ nvim/lua/plugins/notes-slider.lua
@@ -0,0 +1,15 @@
+return {
+    "chrisj0110/notes-slider",
+    config = function()
+        require('notes-slider').setup({
+            horizontal_split_size = 15,
+            vertical_split_size = 70,
+            scratch_file_prefix = "scratch-",
+            -- scratch_file_extension = "md",
+            -- scratch_file_dir = "~/bin",
+        })
+
+        vim.api.nvim_set_keymap('n', '[s', ':lua require("notes-slider").toggle_scratch_using_tmux_name(false, false)<CR>', { noremap = true, silent = true })
+        vim.api.nvim_set_keymap('n', ']s', ':lua require("notes-slider").toggle_scratch_using_tmux_name(true, true)<CR>', { noremap = true, silent = true })
+    end,
+}
