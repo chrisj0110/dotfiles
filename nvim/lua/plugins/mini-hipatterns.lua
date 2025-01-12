fdeeec76ede777ec10f3da8e8869c6a4a1b02fb7
chrisj0110 <45946825+chrisj0110@users.noreply.github.com>
2025-01-12 15:34:11 -0600

    catppuccin color for TODO highlighting

--- nvim/lua/plugins/mini-hipatterns.lua
+++ nvim/lua/plugins/mini-hipatterns.lua
@@ -5,7 +5,8 @@ return {
         local hipatterns = require('mini.hipatterns')
         hipatterns.setup({
             highlighters = {
-                todo = { pattern = '%f[%w]()TODO()%f[%W]',  group = 'MiniHipatternsTodo'  },
+                -- code for custom color figured out from https://github.com/echasnovski/mini.nvim/discussions/1024
+                todo = { pattern = '%f[%w]()TODO()%f[%W]', group = hipatterns.compute_hex_color_group('#fab387', 'fg') },
 
                 -- Highlight hex color strings (`#rrggbb`) using that color
                 hex_color = hipatterns.gen_highlighter.hex_color(),
