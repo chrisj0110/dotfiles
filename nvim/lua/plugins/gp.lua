4ba52cd330d97a35f03cee647e2e8790b9384e63
chrisj0110 <45946825+chrisj0110@users.noreply.github.com>
2025-01-28 13:13:07 -0600

    one window for gp chat

--- nvim/lua/plugins/gp.lua
+++ nvim/lua/plugins/gp.lua
@@ -33,6 +33,7 @@ return {
         -- gp finder:
         vim.keymap.set('n', '<leader>gf', function()
             vim.cmd("vsplit")
+            vim.cmd("on")
             require("telescope.builtin").live_grep({
                 prompt_title = "Search gp chats",
                 ---@diagnostic disable-next-line: undefined-field
@@ -47,15 +48,39 @@ return {
             })
         end, { noremap = true, desc = "GPT Prompt chat finder" })
 
-        vim.keymap.set("n", "<leader>gg", ":GpChatToggle<cr> | :on<cr>", {desc = "GPT Prompt toggle"})
-        vim.keymap.set("n", "<leader>gn", ":GpChatNew vsplit<cr> | :on<cr>", {desc = "GPT Prompt new"})
-        vim.keymap.set("v", "<leader>gn", ":<C-u>'<,'>GpChatNew vsplit<cr> | :on<cr>", {desc = "GPT Prompt new"})
-        vim.keymap.set("v", "<leader>g[", ":<C-u>'<,'>GpChatPaste<cr> | :on<cr>", {desc = "GPT Prompt paste"})
+        local add_default_prompt = function()
+            local default_prompt = "Don't give me any explanations or extra context, just give me the answer. If I need more information I will ask in a follow-up question."
+            vim.cmd("startinsert")
+            vim.api.nvim_put({default_prompt, "", ""}, "l", true, true)
+        end
+
+        vim.keymap.set("n", "<leader>gg", function()
+            vim.cmd("GpChatToggle")
+            vim.cmd("on")
+        end, {desc = "GPT Prompt toggle"})
+
+        vim.keymap.set("n", "<leader>gn", function()
+          vim.cmd("GpChatNew vsplit")
+          vim.cmd("on")
+          add_default_prompt()
+        end, {desc = "GPT Prompt new"})
+
+        vim.keymap.set("v", "<leader>gn", function()
+          vim.cmd([[execute ":'<,'>GpChatNew vsplit"]])
+          vim.cmd("on")
+          add_default_prompt()
+        end, {desc = "GPT Prompt new"})
+
+        vim.keymap.set("v", "<leader>g[", function()
+          vim.cmd([[execute ":'<,'>GpChatPaste vsplit"]])
+          vim.cmd("on")
+        end, {desc = "GPT Prompt paste"})
+
         vim.keymap.set("n", "<leader>go", ":GpAppend<cr>", {desc = "GPT Prompt append (with prompt)"})
         vim.keymap.set("v", "<leader>go", ":GpRewrite<cr>", {desc = "GPT Prompt rewrite selected text (with prompt)"})
         vim.keymap.set("v", "<leader>gi", ":GpImplement<cr>", {desc = "GPT Prompt rewrite selected text (no prompt)"})
 
         -- until I figure out how to make this the default prompt:
-        vim.keymap.set("n", "<leader>gq", "iDon't give me any explanations or extra context, just give me the answer. If I need more information I will ask in a follow-up question.<cr><cr>", {desc = "GPT Prompt quick answer"})
+        -- vim.keymap.set("n", "<leader>gq", "iDon't give me any explanations or extra context, just give me the answer. If I need more information I will ask in a follow-up question.<cr><cr>", {desc = "GPT Prompt quick answer"})
     end,
 }
