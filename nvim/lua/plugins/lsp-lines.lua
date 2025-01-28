aceceedb045769ea4bb5f9f7a0dbb9060ec73f8d
chrisj0110 <45946825+chrisj0110@users.noreply.github.com>
2025-01-28 14:29:08 -0600

    replace lsp-lines with tiny-inline-diagnostic

--- nvim/lua/plugins/lsp-lines.lua
+++
@@ -1,8 +0,0 @@
-return {
-    "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
-    config = function()
-        require("lsp_lines").setup()
-        vim.diagnostic.config { virtual_text = true, virtual_lines = false }
-        vim.keymap.set("n", "gl", require("lsp_lines").toggle, { desc = "Toggle lsp_lines" })
-    end
-}
