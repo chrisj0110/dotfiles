a6f1f7507a9248762d7030586032ea9b056721f2
chrisj0110 <45946825+chrisj0110@users.noreply.github.com>
2025-02-01 14:10:58 -0600

    lazy update and notes-slider breaking changes

--- nvim/lua/plugins/notes-slider.lua
+++ nvim/lua/plugins/notes-slider.lua
@@ -4,14 +4,15 @@ return {
     -- dev = true,
     config = function()
         require('notes-slider').setup({
-            horizontal_split_size = 15,
-            vertical_split_size = 70,
-            scratch_file_prefix = "scratch-",
-            -- scratch_file_extension = "md",
-            -- scratch_file_dir = "~/bin",
+            notes_file_prefix = "scratch-",
+            -- notes_file_extension = "md",
+            -- notes_file_dir = "~/bin",
         })
 
-        vim.api.nvim_set_keymap('n', '[s', ':lua require("notes-slider").toggle_scratch_using_tmux_name(false, false)<CR>', { noremap = true, silent = true })
-        vim.api.nvim_set_keymap('n', ']s', ':lua require("notes-slider").toggle_scratch_using_tmux_name(true, true)<CR>', { noremap = true, silent = true })
+        vim.opt.equalalways = false -- Disable automatic resizing of splits
+
+        vim.api.nvim_set_keymap('n', '[s', ':aboveleft split | wincmd K | resize 15<CR>:lua require("notes-slider").open_notes_using_tmux_session_name()<CR>', { noremap = true, silent = true })
+        vim.api.nvim_set_keymap('n', ']s', ':vsplit | wincmd R | vertical resize 70<CR>:lua require("notes-slider").open_notes_using_tmux_session_name()<CR>', { noremap = true, silent = true })
+        vim.api.nvim_set_keymap('n', '-s', ':lua require("notes-slider").open_notes_using_tmux_session_name()<CR>', { noremap = true, silent = true })
     end,
 }
