9428d629b67606da0082c26b059d1a075deaa488
chrisj0110 <45946825+chrisj0110@users.noreply.github.com>
2025-02-11 14:02:33 -0600

    git_log()

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -26,6 +26,13 @@ return {
                   },
               },
           },
+          git_log = {
+              confirm = function(picker, item)
+                  vim.notify("Copied " .. item.commit .. " to clipboard")
+                  vim.fn.setreg("+", item.commit)
+                  picker:close()
+              end,
+          },
           git_log_file = {
               confirm = function(picker, item)
                   vim.notify("Copied " .. item.commit .. " to clipboard")
@@ -66,6 +73,7 @@ return {
       -- git
       { "<leader>gs", function() Snacks.picker.git_status() end, desc = "Git Status" },
       { "<leader>gd", function() Snacks.picker.git_diff() end, desc = "Git Diff (Hunks)" },
+      { "<leader>gl", function() Snacks.picker.git_log() end, desc = "Git Log" },
       { "<leader>gf", function() Snacks.picker.git_log_file() end, desc = "Git Log File" },
       { "<leader>gB", function() Snacks.picker.git_branches() end, desc = "Git Branches" },
       -- grep
