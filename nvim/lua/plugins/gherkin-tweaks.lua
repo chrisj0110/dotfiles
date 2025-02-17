c7bc35d1bcf10d86f3e3bed0e569a4c7ab8fbb67
chrisj0110 <45946825+chrisj0110@users.noreply.github.com>
2025-02-17 10:02:55 -0600

    add @live tag also

--- nvim/lua/plugins/gherkin-tweaks.lua
+++ nvim/lua/plugins/gherkin-tweaks.lua
@@ -3,21 +3,25 @@ return {
     -- dir = "~/gherkin-tweaks",
     -- dev = true,
     config = function()
-        require('gherkin-tweaks').setup({
-            tag = '@cj',
+        local gherkin_tweaks = require('gherkin-tweaks')
+        gherkin_tweaks.setup({
+            -- tag = '@cj',
             auto_save = true,
         })
 
-        vim.keymap.set('n', '<leader>mi',
-        require('gherkin-tweaks').isolate_test,
-        { desc = 'Isolate just this gherkin test' })
+        vim.keymap.set('n', '<leader>mi', function()
+            gherkin_tweaks.add_tag_to_section("@live")
+            gherkin_tweaks.isolate_test("@cj")
+        end, { desc = 'Isolate just this gherkin test' })
 
-        vim.keymap.set('n', '<leader>mj',
-        require('gherkin-tweaks').add_tag_to_section,
-        { desc = 'Add tag to the top of the current gherkin section' })
+        vim.keymap.set('n', '<leader>mj', function()
+            gherkin_tweaks.add_tag_to_section("@live")
+            gherkin_tweaks.add_tag_to_section("@cj")
+        end, { desc = 'Add tag to the top of the current gherkin section' })
 
-        vim.keymap.set('n', '<leader>me',
-        require('gherkin-tweaks').copy_table_header,
+        vim.keymap.set('n', '<leader>me', function()
+            gherkin_tweaks.copy_table_header()
+        end,
         { desc = 'Copy the previous gherkin table header above the current line' })
     end
 }
