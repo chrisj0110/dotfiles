7188fef7191954bcaa67d98df4153919fdd95903
chrisj0110 <45946825+chrisj0110@users.noreply.github.com>
2025-04-07 12:13:36 -0500

    fix viewing changes in current branch

--- nvim/lua/plugins/diffview.lua
+++ nvim/lua/plugins/diffview.lua
@@ -1,10 +1,9 @@
 return {
     "sindrets/diffview.nvim",
-    dependencies = { "nvim-telescope/telescope.nvim" },
     lazy = true,
     keys = {
         {
-            "<leader>gb", ":tabfirst<CR>:tabonly<CR>:lua require('diffview').open(require('telescope.utils').get_os_command_output({'git', 'merge-base', 'HEAD', 'master'})[1] .. '..HEAD')<CR>"
+            "<leader>gb", ":tabfirst<CR>:tabonly<CR>:lua require('diffview').open('origin/master...HEAD')<CR>"
         },
     },
 }
