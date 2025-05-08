64680a267d8b041159dc60ed0eaecdf43caabe62
chrisj0110 <45946825+chrisj0110@users.noreply.github.com>
2025-05-08 15:44:31 -0500

    bigger lazygit window

--- nvim/lua/plugins/lazygit.lua
+++ nvim/lua/plugins/lazygit.lua
@@ -4,9 +4,11 @@ return {
         "nvim-lua/plenary.nvim",
     },
     config = function()
-        -- optional for floating window border decoration
         vim.g.lazygit_use_custom_config_file_path = 1
         vim.g.lazygit_config_file_path = { os.getenv("HOME") .. "/.config/lazygit/config.yml" }
+
+        vim.g.lazygit_floating_window_winblend = 0  -- Make window fully opaque
+        vim.g.lazygit_floating_window_scaling_factor = 1.0  -- Use full window size
 
         vim.keymap.set("n", "<leader>l", "<cmd>LazyGit<cr>", {desc = "open lazygit (cwd)"})
     end
