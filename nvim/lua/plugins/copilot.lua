6235beb731ea09661ec28f3a834f84d4b505072e
chrisj0110 <45946825+chrisj0110@users.noreply.github.com>
2025-09-29 20:18:41 -0500

    switch to let copilot make suggestions

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,38 +1,37 @@
--- Copilot configuration for manual suggestions only
 return {
   "github/copilot.vim",
   -- event = "VeryLazy",
   config = function()
-    -- Disable automatic suggestions
-    vim.g.copilot_no_tab_map = true
-    vim.g.copilot_assume_mapped = true
-    vim.g.copilot_enabled = true
+    -- -- Disable automatic suggestions
+    -- vim.g.copilot_no_tab_map = true
+    -- vim.g.copilot_assume_mapped = true
+    -- vim.g.copilot_enabled = true
 
-    -- Disable automatic triggers
-    vim.g.copilot_filetypes = {
-      ["*"] = false,
-    }
+    -- -- Disable automatic triggers
+    -- vim.g.copilot_filetypes = {
+    --   ["*"] = false,
+    -- }
 
-    -- Keymaps for manual control
-    vim.keymap.set('i', '<C-]><C-]>', function()
-        -- Create virtual text indicator
-        local ns = vim.api.nvim_create_namespace("copilot_thinking")
-        local bufnr = vim.api.nvim_get_current_buf()
-        local row = vim.api.nvim_win_get_cursor(0)[1] - 1
-
-        vim.api.nvim_buf_set_extmark(bufnr, ns, row, 0, {
-            virt_text = {{"🤖 thinking...", "Comment"}},
-            virt_text_pos = "eol"
-        })
-
-        -- get the suggestion
-        vim.fn['copilot#Suggest']()
-
-        -- Clear the virtual text after delay
-        vim.defer_fn(function()
-            vim.api.nvim_buf_clear_namespace(bufnr, ns, 0, -1)
-        end, 2000)
-    end, { desc = 'Request Copilot suggestion' })
+    -- -- Keymaps for manual control
+    -- vim.keymap.set('i', '<C-]><C-]>', function()
+    --     -- Create virtual text indicator
+    --     local ns = vim.api.nvim_create_namespace("copilot_thinking")
+    --     local bufnr = vim.api.nvim_get_current_buf()
+    --     local row = vim.api.nvim_win_get_cursor(0)[1] - 1
+    --
+    --     vim.api.nvim_buf_set_extmark(bufnr, ns, row, 0, {
+    --         virt_text = {{"🤖 thinking...", "Comment"}},
+    --         virt_text_pos = "eol"
+    --     })
+    --
+    --     -- get the suggestion
+    --     vim.fn['copilot#Suggest']()
+    --
+    --     -- Clear the virtual text after delay
+    --     vim.defer_fn(function()
+    --         vim.api.nvim_buf_clear_namespace(bufnr, ns, 0, -1)
+    --     end, 2000)
+    -- end, { desc = 'Request Copilot suggestion' })
 
     vim.keymap.set('i', '<C-]><C-y>', 'copilot#Accept("\\<CR>")', {
         expr = true,
