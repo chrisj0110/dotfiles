d673764fc0e3ed557c63b2effce3d56b915f5a05
chrisj0110 <45946825+chrisj0110@users.noreply.github.com>
2025-10-24 11:39:36 -0500

    snacks upgrade

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -73,6 +73,7 @@ return {
       { "<leader>gs", function() Snacks.picker.git_status() end, desc = "Git Status" },
       { "<leader>gd", function() Snacks.picker.git_diff() end, desc = "Git Diff (Hunks)" },
       { "<leader>gl", function() Snacks.picker.git_log() end, desc = "Git Log" },
+      { "<leader>gL", function() Snacks.picker.git_log_line() end, desc = "Git Log Line" },
       { "<leader>gf", function() Snacks.picker.git_log_file() end, desc = "Git Log File" },
       { "<leader>gB", function() Snacks.picker.git_branches() end, desc = "Git Branches" },
       -- grep
