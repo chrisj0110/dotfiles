412ec3bc8bc02d284169643575be12c08ff8c235
chrisj0110 <45946825+chrisj0110@users.noreply.github.com>
2025-11-13 13:26:58 -0600

    Revert "add code block"
    This reverts commit 31bac9aaf63f002862f2913534c8100007220488.

--- nvim/lua/plugins/copilotchat.lua
+++ nvim/lua/plugins/copilotchat.lua
@@ -28,17 +28,7 @@ return {
           vim.cmd("wincmd k")
           -- add file path context to copilot chat
           vim.api.nvim_put({ "#file:" .. filepath, "", "", "" }, 'c', true, true)
-        end, desc = "Send file context to CopilotChat"
-      },
-      { "<leader>ct", function()
-          vim.api.nvim_put({ "", "```", "", "```" }, 'c', true, true)
-
-          vim.defer_fn(function() -- gotta sleep a bit here
-              vim.cmd("normal! k")
-              vim.cmd("startinsert")
-          end, 100) -- ms
-        end, desc = "add code block"
-      },
+      end, desc = "Send file context to CopilotChat" }
     },
   },
 }
