7e1542098c8daaa59bfcc1202392b6c7d0806908
chrisj0110 <45946825+chrisj0110@users.noreply.github.com>
2025-11-22 14:25:30 -0600

    open window on the right

--- nvim/lua/plugins/codecompanion.lua
+++ nvim/lua/plugins/codecompanion.lua
@@ -8,6 +8,13 @@ return {
         opts = {
             log_level = "DEBUG", -- or "TRACE"
         },
+        display = { -- https://codecompanion.olimorris.dev/configuration/chat-buffer
+            chat = {
+                window = {
+                    position = "right",
+                },
+            },
+        },
     },
     keys = {
         { "<leader>cc", "<cmd>CodeCompanionChat Toggle<CR>", { desc = "Toggle CodeCompanion Chat" } }
