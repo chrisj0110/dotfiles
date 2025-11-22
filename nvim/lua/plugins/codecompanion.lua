71178886a7611f0b66f17866f05949bb6a1445c4
chrisj0110 <45946825+chrisj0110@users.noreply.github.com>
2025-11-22 14:21:10 -0600

    fmt

--- nvim/lua/plugins/codecompanion.lua
+++ nvim/lua/plugins/codecompanion.lua
@@ -1,16 +1,16 @@
 return {
-  "olimorris/codecompanion.nvim",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-treesitter/nvim-treesitter",
-  },
-  opts = {
-    opts = {
-      log_level = "DEBUG", -- or "TRACE"
+    "olimorris/codecompanion.nvim",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-treesitter/nvim-treesitter",
     },
-  },
-  keys = {
-      { "<leader>cc", "<cmd>CodeCompanionChat Toggle<CR>", { desc = "Toggle CodeCompanion Chat" } }
-  },
+    opts = {
+        opts = {
+            log_level = "DEBUG", -- or "TRACE"
+        },
+    },
+    keys = {
+        { "<leader>cc", "<cmd>CodeCompanionChat Toggle<CR>", { desc = "Toggle CodeCompanion Chat" } }
+    },
 }
 
